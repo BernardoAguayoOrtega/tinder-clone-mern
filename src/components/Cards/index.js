722a50be944cff2d37d1d99f913f62148058cebc
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-15 12:46:56 -0500

    add logic to card component

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,12 +1,48 @@
-//import react
-import React from 'react';
+//import react and its hooks
+import React, { useState } from 'react';
+//import TinderCard
+import TinderCard from 'react-tinder-card';
 
 //create cards component and export it
 export const Cards = () => {
+	const [people, setPeople] = useState([
+		{
+			name: 'Hercules',
+			url:
+				'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/dog_cool_summer_slideshow/1800x1200_dog_cool_summer_other.jpg?resize=750px:*',
+		},
+		{
+			name: 'Lucas',
+			url:
+				'https://www.guidedogs.org/wp-content/uploads/2019/11/website-donate-mobile.jpg',
+		},
+	]);
+
+	//swipe function
+	const swiped = (direction, nameToDelete) => {
+		console.log(`removing ${nameToDelete}`);
+		//setLastDirection(direction);
+	};
+
+	//out frame function
+	const outOfFrame = (name) => {
+		console.log(`${name} left the screen`);
+	};
+
 	return (
-		<>
-			<h1>I'm a card</h1>
-			<p>component</p>
-		</>
+		<div className='cards'>
+			<div className='cards__cardContainer'>
+				{people.map((person) => (
+					<TinderCard
+						className='swipe'
+						key={person.name}
+						preventSwipe={['upp', 'down']}
+						onSwipe={(dir) => swiped(dir, person.name)}
+						onCardLeftScreen={() => outOfFrame(person.name)}>
+						{person.name}
+					</TinderCard>
+				))}
+			</div>
+		</div>
 	);
 };
