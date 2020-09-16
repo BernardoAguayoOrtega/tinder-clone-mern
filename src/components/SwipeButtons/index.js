83bcfe9a413e6775465a573972a0283aabe662f7
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-16 08:40:49 -0500

    import icons from material ui icons to swipe buttons component

--- src/components/SwipeButtons/index.js
+++ src/components/SwipeButtons/index.js
@@ -1,7 +1,18 @@
 //import react
 import React from 'react';
+//import material ui components
+import ReplayIcon from '@material-ui/icons/Replay';
+import CloseIcon from '@material-ui/icons/Close';
+import StarRateIcon from '@material-ui/icons/StarRate';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FLashOnIcon from '@material-ui/icons/FlashOn';
+import IconButton from '@material-ui/core/IconButton';
 
 //create swipe buttons component and export it
 export const SwipeButtons = () => {
-	return <h1>I'm the swipe component</h1>;
+	return (
+		<div className='swipe-buttons'>
+			<h1>swipe buttons</h1>
+		</div>
+	);
 };
