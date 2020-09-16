be78929dfd3af7d24bd6eefb05d1062b85a5eb9d
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-16 08:17:03 -0500

    create swipe buttons component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,6 +4,8 @@ import React from 'react';
 import { Header } from '../Header';
 //import cards
 import { Cards } from '../Cards';
+//import swipe buttons component
+import { SwipeButtons } from '../SwipeButtons';
 
 //export and create app component
 export const App = () => {
@@ -14,6 +16,7 @@ export const App = () => {
 			{/*Tinder cards*/}
 			<Cards />
 			{/*Swipe buttons*/}
+			<SwipeButtons />
 		</>
 	);
 };
---
+++ src/components/SwipeButtons/index.js
@@ -0,0 +1,7 @@
+//import react
+import React from 'react';
+
+//create swipe buttons component and export it
+export const SwipeButtons = () => {
+	return <h1>I'm the swipe component</h1>;
+};
