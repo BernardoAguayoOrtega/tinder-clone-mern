8a7c9e219071695a230fb705926341cfa854f88e
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-16 19:14:17 -0500

    Updates

---
+++ precache-manifest.22b8737e425609fa628ce46f02f33555.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8b3886f7a2c57f84412da639f12c38f",
+    "url": "/tinder-clone-mern/index.html"
+  },
+  {
+    "revision": "66514ca17a56f039aea0",
+    "url": "/tinder-clone-mern/static/css/main.5de483d3.chunk.css"
+  },
+  {
+    "revision": "4883439fad943ece5183",
+    "url": "/tinder-clone-mern/static/js/2.6868c70b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tinder-clone-mern/static/js/2.6868c70b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66514ca17a56f039aea0",
+    "url": "/tinder-clone-mern/static/js/main.1e0850a0.chunk.js"
+  },
+  {
+    "revision": "2f386ed48c6a544bd8f9",
+    "url": "/tinder-clone-mern/static/js/runtime-main.84900d2d.js"
+  }
+]);
